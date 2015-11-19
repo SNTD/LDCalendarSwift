9a3d6be3d7dd22502981caf4443cb6daada2d38f
lidi <1078805781@qq.com>
2015-11-19 14:55:10 +0800

    Init Commit

---
+++ LDCalendarSwift/LDCalendar/UIImageExtend.swift
@@ -0,0 +1,13 @@
+//
+//  UIImageExtend.swift
+//  LDCalendarSwift
+//
+//  Created by lidi on 11/19/15.
+//  Copyright © 2015 lidi. All rights reserved.
+//
+
+import UIKit
+
+class UIImageExtend: NSObject {
+
+}
